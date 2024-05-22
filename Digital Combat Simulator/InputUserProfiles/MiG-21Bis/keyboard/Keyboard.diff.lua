e4205ace7bf248608a6fd496dd5c6e9059a2c38d
whartsell <will@willshouse.net>
2024-05-21 19:36:01 -0700

    initial commit for mig-21

---
+++ Digital Combat Simulator/InputUserProfiles/MiG-21Bis/keyboard/Keyboard.diff.lua
@@ -0,0 +1,29 @@
+local diff = {
+	["keyDiffs"] = {
+		["d3191pnilu3191cd42vd1vpnilvu0"] = {
+			["name"] = "Fire Gun",
+			["removed"] = {
+				[1] = {
+					["key"] = "Space",
+				},
+			},
+		},
+		["d83pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Space",
+				},
+			},
+			["name"] = "Eject (3 times)",
+			["removed"] = {
+				[1] = {
+					["key"] = "E",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+		},
+	},
+}
+return diff
